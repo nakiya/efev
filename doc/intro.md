471b3532ab49824bea95f01e278a2635060f8362
nakiya <dumrat@gmail.com>
2021-01-17 19:35:46 +0800

    Mostly done with evaluation. Added documentation.

--- doc/intro.md
+++ doc/intro.md
@@ -1,3 +1,89 @@
-# Introduction to efev
+# efev - An Introduction
 
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
+## Problem:
+
+Create a web grid demo focusing on formula evaluation. Following are the aspects with which this demo is concerned.
+
+1. Present to user with a js/react/whatever grid to input data into. Ideally, this should have dimensions of 15 x 26. We can use a smaller grid at the beginning.
+
+2. User should be able to input strings into each cell (except row/col headers). These strings can represent numbers `(42, 3.14, ...)`, strings `("Hello", ...)` or formulas `(= 1 + 2.4, =SQRT(SUM(3,5,6)))`. These formulas can include cell references like this: `=A.2 + 23`. In the previous formula, there's a reference to the value in the cell identified by row 1 and column 'A'. Formulas can also include cell range references like this: `=SUM(A.2:A.6)`. The previous formula designates a sum of the values for cells between and including `A.2` and `A.6`. In addition to this, formulas can include operators `(+,-,*,/,%,~,...)` and custom functions `(SUM, SQRT, SIN, CONCATENATE, ...)`. (There are a host of other UI functionalities related to cell editing that we will not cover here. We are only concerned about the logic of formula evaluation.)
+
+3. There are options on when and where to evaluate formulas:
+    1. Provide an "Evaluate" button to the user. User can edit multiple cells on a "dumb" grid and when he clicks on the button, evaluation for any and all formulas in the grid can happen. This evaluation can happen on the front end itself or at the back end. For the purpose of this demo, we will consider this option only.
+    2. Evaluate formulas "as they happen". When a user enters a value into a cell, the front end would immediately try to evaluate it in case it's a formula. Optionally, we can consider doing this kind of immediate evaluation at back end though it may happen to be disruptive to the user experience.
+
+## Formula evaluation
+
+We can think of the grid as a set of variables. A 2x2 grid having cells `A.1`, `A.2`, `B.1`, `B.2` can be represented with 4 variables having the same names. Hence, if these cells respectively contain the following values: `"2", "=A.1 + 5", "= SUM(A.1, A.2)", "= B.1"`, we can represent this grid with the following program:
+
+#### Program-1
+```
+A.1 = 2;
+A.2 = A.1 + 5;
+B.1 = SUM(A.1, A.2);
+B.2 = B.1;
+```
+
+(Let's ignore the mechanism with which we converted the cell value string in `A.1` into an integer and in `A.2` into a formula and so on for now.)
+
+This shows that the grid can be serialized as this program and can be sent back and forth among front end and back end. Additionally, this means that if we have a way, we can evaluate this program anywhere and figure out the cell values of the grid. Basically this is an exercise in interpreting a program.
+
+### Parsing
+
+We use [instaparse library](https://github.com/Engelberg/instaparse) for generating a parser to parse our formula language. The parser grammar at the moment of writing this document is below:
+
+```
+"prog = assign+;
+    assign = spaces cellref spaces <'='> spaces expr spaces <';'>;
+    <expr> = add-sub;
+    <add-sub> = mult-div | add | sub;
+    add = add-sub spaces <'+'> spaces mult-div;
+    sub = add-sub spaces <'-'> spaces mult-div;
+    <mult-div> = factor | mult | div;
+    mult = mult-div spaces <'*'> spaces factor;
+    div = mult-div spaces <'/'> spaces factor;
+    <factor> = funcall | number | cellref | <'('> spaces expr spaces <')'>;
+    <spaces> = <#'\\s*'>;
+    number = #'^[-~]?(0|[1-9]\\d*)?(\\.\\d+)?(?<=\\d)';
+    cellref = colid <'.'> rowid;
+    <rowid> = #'[0-9]+' | ident;
+    <colid> = ident;
+    <ident> = #'[a-zA-Z]\\w*';
+    <funname> = ident;
+    funcall = funname spaces <'('> arglist <')'> spaces;
+    arglist = (spaces expr spaces <','> spaces)* expr | cellrange | Epsilon;
+    cellrange = cellref <':'> cellref;"
+```
+
+If we parse [Program-1](#Program-1), we get the following parse tree:
+
+![](parse-tree.png)
+
+### Evaluation
+
+We have the parse tree and now we should be able to evaluate this tree using a tree walking algorithm. But there's some issues:
+
+1. Cell references and cell range references mean that some cells have to be evalauated for value before others. There's an order of evaluation.
+2. We need to handle self references in formulas. i.e. `A.2 = A.2 + 5;`
+3. We need to handle simple or complicated circular references between cells. i.e. `A.1 = A.2 + 3; A.2 = B.1 - 5; B.1 = A.1;`
+
+This brings us to the first necessary step:
+
+#### Topological sorting of cells
+
+We need to find the topological ordering of cell references so we can evaluate each cell in that order. This can be done with a topological sorting algorithm. In our example, the cell reference dependencies are as below and have to be executed in that order. i.e. `A.1, A.2, B.1, B.2`.
+
+Topological sorting algorithms can detect cycles and self references. So, this algorithm would take care of those situations.
+
+For this demo, we use the [dependency library](https://github.com/stuartsierra/dependency) for generating the topological sorting for cell references for a given program.
+
+![](eval-order.png)
+
+#### Evaluation
+
+Once we have the parse tree and the cell evaluation order, evaluation has two main steps:
+
+1. Sort the cell assign statements (`assign` in parser) which are immediate children of the top level form `prog` according to the cell evaluation order we discovered.
+2. Evaluate each assign statement using a tree post-walk algorithm while keeping track of the cell value changes.
+
+This would present us with a new state for the grid. We can serialize this state and send back to the front end which in turn will display the new state.
--- src/duminda/efev.clj
+++ src/duminda/efev.clj
@@ -1,15 +1,18 @@
 (ns duminda.efev
   (:require [instaparse.core :as insta]
-            [clojure.edn :as edn]
             [com.stuartsierra.dependency :as dep]
-            [clojure.walk :refer [prewalk postwalk]]))
+            [clojure.walk :refer [prewalk postwalk]]
+            [rhizome.viz :as viz]
+            [clojure.set :as cs]))
 
 ;;Incomplete. But this suffices as a PoC
+;;TODO: Doesn't handle strings yet. Do we need formulas with strings in them?
+;;TODO: Use of semicolon as a statement terminator will cause issue. One solution might be to send seperate formulas, parse individually and then combine into a single tree.
 (def formula-grammar
                                         ;;;from here and modified: https://notebooks.gesis.org/binder/jupyter/user/bhugueney-binder-test-y69wi9ts/notebooks/Clojure.ipynb : lang0-parser
                                         ;;;number regex here: https://stackoverflow.com/questions/2811031/decimal-or-numeric-values-in-regular-expression-validation/39399503#39399503
-  "prog = stmt+;
-    stmt = spaces cellref spaces <'='> spaces expr spaces <';'>;
+  "prog = assign+;
+    assign = spaces cellref spaces <'='> spaces expr spaces <';'>;
     <expr> = add-sub;
     <add-sub> = mult-div | add | sub;
     add = add-sub spaces <'+'> spaces mult-div;
@@ -29,51 +32,7 @@
     arglist = (spaces expr spaces <','> spaces)* expr | cellrange | Epsilon;
     cellrange = cellref <':'> cellref;")
 
-(def formula-parser (insta/parser formula-grammar))
-
-(comment
-  (formula-parser "2 + 3 / 5" :trace true)
-  (formula-parser "A.1 = 2 + 3;")
-  (formula-parser "A.1 = 2 + 3; B.1 = A.1 + 2;")
-  (insta/disable-tracing!)
-  ,)
-
-(comment
-  (insta/visualize (formula-parser "A.1 = SQRT(SUM(78 + A.12) + B.22); B.1 = A.1 + 3;"))
-  ;; (insta/visualize (formula-parser "SUM(1,2, 3 , SQRT(SUM(9,8))) + SUM()"))
-  ;; (insta/visualize (formula-parser "2 + 3 * (24 -5)"))
-  ;; (insta/visualize (formula-parser "2 + 3 / 5"))
-  ;; (insta/visualize (formula-parser "5 * 2 - 6"))
-  ;; (insta/visualize (formula-parser "SUM(A.1, A.2, B.1, B.2) * 12"))
-  ;; (insta/visualize (formula-parser "1.34 + 3.45"))
-  ;; (insta/visualize (formula-parser "-0.3 * .56 + -.7"))
-  ;; (insta/visualize (formula-parser "SUM(A.1:B.5)"))
-  ;; (insta/visualize (formula-parser "SUM(t2.Notional:t4.Notional)"))
-  ;; (insta/visualize (formula-parser "45 + ~34"))
-  )
-
-;; Model is a map here.
-(def model {"A.1"         1    "A.2"         2    "A.3"         3 "A.4" 4
-            "B.1"         5    "B.2"         6    "B.3"         7 "B.4" 8
-            "t1.Notional" 1000 "t2.Notional" 2000 "t3.Notional" 3000})
-
-(def custom-functions
-  {"SUM"    +
-   "SQRT"   #(Math/sqrt %)
-   "CONCAT" str})
-
-(defn lookup-cell
-  ([col row]
-   (model (str col "." row)))
-  ([[_ col row]]
-   (println "lookup-cell 2nd called")
-   (lookup-cell col row)))
-
-(defn call-function
-  "Form of the second argument: [:arglist 80 54]. So we destructure here and drop the first item"
-  [funname [_ & args :as orig-args]]
-  (println "call-function:" funname "with" orig-args "args = " args)
-  (apply (custom-functions funname) (flatten args)))
+(insta/defparser basic-parser formula-grammar)
 
 (defn expand-range [[_ start-col start-row] [_ end-col end-row]]
                                         ;(println "unroll-range" start-col "." start-row " to " end-col "." end-row)
@@ -94,7 +53,7 @@
           row-range                (map str (range begin-row-i (inc term-row-i)))]
       (->> (for [col col-range row row-range]
              [col row])
-           (map #(apply lookup-cell %))))))
+           (map #(vector :cellref (first %) (second %)))))))
 
 (comment
   (expand-range [:cellref "A" "1"] [:cellref "B" "1"])
@@ -102,25 +61,45 @@
   (expand-range [:cellref "B" "1"] [:cellref "A" "3"])
   ,)
 
-(def transform-map {:number    edn/read-string
-                    :add       +        :sub   - :mult * :div /
-                    :colid     identity :rowid identity
-                    :cellref   lookup-cell
-                    :cellrange expand-range
-                    :funcall   call-function
-                    :prog      identity})
+(defn expand-range-inline [parse-tree]
+  (->> parse-tree
+       (prewalk (fn [n] (if (and (vector? n) (= (first n) :cellrange))
+                          (expand-range (nth n 1) (nth n 2))
+                          n)))
+       (prewalk (fn [n] (if (and (vector? n) (= (first n) :arglist) (seq? (second n)))
+                          (into [:arglist] (second n))
+                          n)))))
+
+(def formula-parser (comp expand-range-inline basic-parser))
+
+(comment
+  (formula-parser "A.1 = 2 + 3 / 5;" :trace true)
+  (formula-parser "A.1 = A.2;")
+  (formula-parser "A.1 = 5.3 + 3;")
+  (formula-parser "A.1 = SUM(B.1:B.3);")
+  (formula-parser "A.1 = SUM(B.1:D.4);")
+  (insta/disable-tracing!)
+  ,)
+
+(comment
+  (insta/visualize (formula-parser "A.1 = SQRT(SUM(78 + A.12) + B.22); B.1 = A.1 + 3;"))
+  (insta/visualize (formula-parser "A.1 = 2; A.2 = A.1 + 5; B.1 = SUM(A.1, A.2); B.2 = B.1;"))
+  ;; (insta/visualize (formula-parser "SUM(1,2, 3 , SQRT(SUM(9,8))) + SUM()"))
+  ;; (insta/visualize (formula-parser "2 + 3 * (24 -5)"))
+  ;; (insta/visualize (formula-parser "2 + 3 / 5"))
+  ;; (insta/visualize (formula-parser "5 * 2 - 6"))
+  ;; (insta/visualize (formula-parser "SUM(A.1, A.2, B.1, B.2) * 12"))
+  ;; (insta/visualize (formula-parser "1.34 + 3.45"))
+  ;; (insta/visualize (formula-parser "-0.3 * .56 + -.7"))
+  ;; (insta/visualize (formula-parser "SUM(A.1:B.5)"))
+  ;; (insta/visualize (formula-parser "SUM(t2.Notional:t4.Notional)"))
+  ;; (insta/visualize (formula-parser "45 + ~34"))
+  )
+
 
 (comment
   (formula-parser "A.1 = SQRT(SUM(A.1:B.3) + 24);")
-  ,)
 
-;;We can use zippers or tree walkers here. But instaparse's built in fn suffices here.
-(comment
-  (insta/transform transform-map (formula-parser "A.1 = SQRT(SUM(A.1:B.3) + 24);"))
-  (insta/transform transform-map (formula-parser "B.1 = SUM(A.1, A.2, B.1, B.2) * 12;"))
-  ,)
-
-(comment
   (def g1 (-> (dep/graph)
               (dep/depend :b :a)
               (dep/depend :c :b)
@@ -129,26 +108,24 @@
 
   (dep/topo-sort g1)
 
-  ,)
-
-(comment
   (formula-parser "A.1 = 2 + 3; B.1 = A.1 + 2;")
-;; => [:prog [:stmt [:cellref "A" "1"] [:add [:number "2"] [:number "3"]]] [:stmt [:cellref "B" "1"] [:add [:cellref "A" "1"] [:number "2"]]]]
+  ;; => [:prog [:assign [:cellref "A" "1"] [:add [:number "2"] [:number "3"]]] [:assign [:cellref "B" "1"] [:add [:cellref "A" "1"] [:number "2"]]]]
 
   (tree-seq (fn [node]
               (println node)
               (if (vector? node)
                 (not= (first node) :cellref)
                 false)) seq
-            [:stmt [:cellref "B" "1"] [:add [:cellref "A" "1"] [:number "2"]]])
+            [:assign [:cellref "B" "1"] [:add [:cellref "A" "1"] [:number "2"]]])
 
   (->> '( 2 (7 nil nil) (88 (5 nil nil) nil) )
        (tree-seq list? rest))
+
   ,)
 
 (defmulti get-dependencies first)
 
-(defmethod get-dependencies :stmt [[_ assign-cell expr]]
+(defmethod get-dependencies :assign [[_ assign-cell expr]]
   [assign-cell (filter #(and (vector? %) (= :cellref (first %)))
                        (tree-seq (fn [node]
                                    (if (vector? node)
@@ -157,20 +134,138 @@
 
 (defmethod get-dependencies :prog [prog]
   (map get-dependencies
-       (filter #(= :stmt (and (vector? %) (first %))) prog)))
+       (filter #(= :assign (and (vector? %) (first %))) prog)))
 
-(defn get-eval-order [parse-tree]
+(defn cell-deps [parse-tree]
+  "Get cell dependencies"
   (->> (get-dependencies parse-tree)
        (mapcat (fn [[assign-cell dep-cells]]
                  (map (fn [d] (vector assign-cell d)) dep-cells)))
        (remove empty?)
-       (reduce (fn [g [a b]] (dep/depend g a b)) (dep/graph))
-       (dep/topo-sort)))
+       (reduce (fn [g [a b]] (dep/depend g a b)) (dep/graph))))
+
+(defn get-eval-order [deps]
+  "Get the topological ordering of deps"
+  (dep/topo-sort deps))
+
+(defn visualize-dependencies [deps]
+  "Show the dependency graph as a graphviz graph."
+  (let [dep-g     (->> deps
+                       :dependencies
+                       (map (fn [[k v]] (vector k (into [] v))))
+                       (into {})
+                       (prewalk #(if (and (vector? %) (= (first %) :cellref)) (str (nth % 1) "." (nth % 2)) %)))
+        dist      (->> (seq dep-g)
+                       flatten
+                       distinct
+                       (into #{}))
+        dep-g-ks  (into #{} (keys dep-g))
+        missing   (cs/difference dist dep-g-ks)
+        missing-m (reduce (fn [m k] (assoc m k [])) {} missing)
+        dep-g     (merge dep-g missing-m)]
+    (viz/view-graph (keys dep-g) dep-g :node->descriptor (fn [n] {:label n}))
+    dep-g))
 
 (comment
-  (get-dependencies [:stmt [:cellref "B" "1"] [:add [:cellref "A" "1"] [:number "2"]]])
-  (get-eval-order
-    (formula-parser "A.1 = 2; A.2 = A.1 + 5; B.1 = A.1 + A.2; B.2 = B.1;"))
-;; => #com.stuartsierra.dependency.MapDependencyGraph{:dependencies {[:cellref "A" "2"] #{[:cellref "A" "1"]}, [:cellref "B" "1"] #{[:cellref "A" "1"] [:cellref "A" "2"]}, [:cellref "B" "2"] #{[:cellref "B" "1"]}}, :dependents {[:cellref "A" "1"] #{[:cellref "A" "2"] [:cellref "B" "1"]}, [:cellref "A" "2"] #{[:cellref "B" "1"]}, [:cellref "B" "1"] #{[:cellref "B" "2"]}}}
+  (cell-deps
+    (formula-parser "A.1 = 2; A.2 = A.1 + 5; B.1 = SUM(A.1, A.2); B.2 = B.1;"))
+
+  ;; => #com.stuartsierra.dependency.MapDependencyGraph{:dependencies {[:cellref "A" "2"] #{[:cellref "A" "1"]}, [:cellref "B" "1"] #{[:cellref "A" "1"] [:cellref "A" "2"]}, [:cellref "B" "2"] #{[:cellref "B" "1"]}}, :dependents {[:cellref "A" "1"] #{[:cellref "A" "2"] [:cellref "B" "1"]}, [:cellref "A" "2"] #{[:cellref "B" "1"]}, [:cellref "B" "1"] #{[:cellref "B" "2"]}}}
+
+  ;; Note that A.3 does not appear in the graph. Basically we don't care about the evaluation order of A.3.
+  (visualize-dependencies (cell-deps
+                            (formula-parser "A.1 = 2; A.2 = A.1 + 5; B.1 = SUM(A.1, A.2); B.2 = B.1;")))
+  ;; => {"A.1" ["A.2" "B.3" "B.1"], "A.2" ["B.1"], "B.1" ["B.2"], "B.2" ["B.3"], "B.3" []}
+
+  (visualize-dependencies (cell-deps
+                            (formula-parser "C.1 = SUM(A.1:A.3); A.1=A.2; A.3=A.4+A.2;")))
+
+  (get-eval-order (cell-deps (formula-parser "A.1 = 2; A.2 = A.1 + 5; B.1 = SUM(A.1, A.2); B.2 = B.1;")))
+;; => ([:cellref "A" "1"] [:cellref "A" "2"] [:cellref "B" "1"] [:cellref "B" "2"] [:cellref "B" "3"])
+
+  ;Self reference
+  (cell-deps (formula-parser "A.1 = A.1;"))
+
+  ;Circular dependency
+  (cell-deps (formula-parser "A.1 = B.1; A.2 = A.1; B.1 = A.2;"))
+
+  (let [parse-tree (formula-parser "A.1 = 2; A.2 = A.1 + 5; B.1 = SUM(A.1, A.2); B.2 = B.1;")
+        eval-order (get-eval-order (cell-deps parse-tree))]
+    [parse-tree eval-order])
+  ;; => [[:prog [:assign [:cellref "A" "1"] [:number "2"]] [:assign [:cellref "A" "2"] [:add [:cellref "A" "1"] [:number "5"]]] [:assign [:cellref "B" "1"] [:funcall "SUM" [:arglist [:cellref "A" "1"] [:cellref "A" "2"]]]] [:assign [:cellref "B" "2"] [:cellref "B" "1"]]] ([:cellref "A" "1"] [:cellref "A" "2"] [:cellref "B" "1"] [:cellref "B" "2"])]
+
+  ,)
+
+
+(def custom-functions
+  {"SUM"    +
+   "SQRT"   #(Math/sqrt %)
+   "CONCAT" str})
+
+(defn lookup-cell-fn [model]
+  (fn lookup-cell
+    ([col row]
+     (println "lookup-cell: " (str col "." row) ", returned: " (@model (str col "." row)))
+     (@model (str col "." row)))
+    ([[_ col row]]
+     (println "lookup-cell 2nd called")
+     (lookup-cell col row))))
+
+(defn call-function
+  "Form of the second argument: [:arglist 80 54]. So we destructure here and drop the first item"
+  [funname [_ & args :as orig-args]]
+  (println "call-function:" funname "with" orig-args "args = " args)
+  (apply (custom-functions funname) (flatten args)))
+
+(defn get-transform-map [model]
+  {:number         #(Integer/parseInt %)
+   :add            (fn [& args]
+                     (println "+ called with : " args)
+                     (apply + args))
+   :sub            -
+   :mult           *
+   :div            /
+   :colid          identity
+   :rowid          identity
+   :cellref        (lookup-cell-fn model)
+   :funcall        call-function
+   :prog           identity
+   :cellref-assign #(str %1 "." %2)
+   :assign         (fn [k v]
+                     (println "Called assign with : " k ": " v)
+                     (swap! model assoc k v))
+   })
+
+(defn eval-tree [parse-tree initial-model]
+  "Evaluates a parse tree with supplied model."
+  (let [model              (atom initial-model)
+        eval-order         (get-eval-order (cell-deps parse-tree))
+        eval-order-map     (apply hash-map (interleave eval-order (range (count eval-order))))
+        statements         (filter #(and (vector? %) (= :assign (first %))) parse-tree)
+        ordered-statements (sort-by #(eval-order-map (second %)) statements)
+        ;;Convert assignment cell to diff type
+        ordered-statements (map (fn [[stmt [kw col row :as cr] expr]]
+                                  [stmt [:cellref-assign col row] expr]) ordered-statements)
+        transform-map      (get-transform-map model)]
+    (doall (map (fn [assign]
+                  (postwalk
+                    (fn [node]
+                      (if (vector? node)
+                        (let [f (transform-map (first node))
+                              _ (println "Evaluating " node "with " f)]
+                          (if f (apply f (rest node)) node))
+                        node)
+                      ) assign))
+                ordered-statements))
+    @model))
+
+(comment
+  ;; Model is a map here.
+  (def test-model {"A.1" 1 "A.2" 2 "A.3" 3 "A.4" 4
+                   "B.1" 5 "B.2" 6 "B.3" 7 "B.4" 8})
+
+  (eval-tree (formula-parser "A.2= 5;C.1 = SUM(A.1:A.3); A.1=A.2; A.3=A.4+A.2;") test-model)
+;; => {"B.4" 8, "A.2" 5, "B.1" 5, "A.1" 5, "B.2" 6, "A.4" 4, "A.3" 9, "B.3" 7, "C.1" 19}
+
 
   ,)
